25ea1a150fed4ba23def4db640b7c7b1bca0cdc5
Klaus <klaus_hybrid33@yahoo.com>
2023-03-16 21:03:04 +0330

    added new functionality

--- server.py
+++ server.py
@@ -1,18 +1,36 @@
 from abc import ABC, abstractmethod
+import threading
+from xmlrpc.server import SimpleXMLRPCServer
+from xmlrpc.client import ServerProxy
+
+from broker import Broker
 
 class Server(ABC):
     @abstractmethod
-    def connect(self):
+    def listen(self, address):
         pass
 
     @abstractmethod
-    def disconnect(self):
+    def close(self):
         pass
 
-    @abstractmethod
-    def send(self, data):
-        pass
+class RPCServer(Server):
+    def __init__(self, broker: Broker, address: tuple[str, int]):
+        self.broker = broker
+        self.server = SimpleXMLRPCServer(address)
+        self.client = ServerProxy('http://localhost:8000')
 
-    @abstractmethod
-    def receive(self):
-        pass
+    def listen(self, address = None):
+        if address is not None:
+            self.server = SimpleXMLRPCServer(address)
+        self.server.register_instance(self.broker)
+        self.server_thread = threading.Thread(target=self.server.serve_forever)
+        self.server_thread.start()
+
+    def close(self):
+        self.server.shutdown()
+        self.server.server_close()
+        self.server_thread.join()
+
+    def call(self, method, *args):
+        return getattr(self.client, method)(*args)
