2b56ec981e4fa38f9ee842c8fc507ff4b19de082
Klaus <klaus_hybrid33@yahoo.com>
2023-03-16 21:35:50 +0330

    more general purpose, could be used to serve topics as well

--- server.py
+++ server.py
@@ -15,15 +15,15 @@ class Server(ABC):
         pass
 
 class RPCServer(Server):
-    def __init__(self, broker: Broker, address: tuple[str, int]):
-        self.broker = broker
+    def __init__(self, handler, address: tuple[str, int]):
+        self.handler = handler
         self.server = SimpleXMLRPCServer(address)
         self.client = ServerProxy('http://localhost:8000') # Dummy client
 
     def listen(self, address = None):
         if address is not None:
             self.server = SimpleXMLRPCServer(address)
-        self.server.register_instance(self.broker)
+        self.server.register_instance(self.handler)
         self.server_thread = threading.Thread(target=self.server.serve_forever)
         self.server_thread.start()
 
