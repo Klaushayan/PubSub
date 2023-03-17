0f377cd086c82ade2590ad100d692400bc16ec6d
Klaus <klaus_hybrid33@yahoo.com>
2023-03-17 19:32:32 +0330

    better main

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 
 
+import signal
+import sys
 from broker import Broker
 from server import RPCServer
 
+def signal_handler(signal, frame):
+    print('Terminating server...')
+    rpc_server.close()
+
+rpc_server = RPCServer(Broker(), ('localhost', 8000))
 
 if __name__ == "__main__":
-    rpc_server = RPCServer(Broker(), ('localhost', 8000))
+    signal.signal(signal.SIGINT, signal_handler)
+    signal.signal(signal.SIGTERM, signal_handler)
     rpc_server.listen()
 
